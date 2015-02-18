b41dea1a6c643b0379dfab2988fc35cbf0b9fe98
dserbarinov <dserbarinov@enernoc.com>
2015-02-18 17:24:45 -0500

    Create a build script to minify sources for distribution

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -7,9 +7,23 @@
 	module.exports = function (grunt) {
 	
 		grunt.loadNpmTasks ('grunt-bower-install');
-
+		grunt.loadNpmTasks ('grunt-dom-munger');
+		grunt.loadNpmTasks ('grunt-contrib-uglify');
+		grunt.loadNpmTasks ('grunt-contrib-clean');
+		grunt.loadNpmTasks ('grunt-contrib-htmlmin');
+		
 		//Project configuration
 		grunt.initConfig ({
+			//I did not use a yeoman template, so no need to call these vars like I did:
+			variables: {
+				dist: 'dist'
+			},
+			
+			clean: {
+				client: ['<%= variables.dist %>']
+			},
+			
+			// enriches the 'bower:js' block in index.html with references to bower components 	
 			bowerInstall: {
 				client: {
 					src: ['index.html'],
@@ -18,11 +32,63 @@
 						'firebase-simple-login'
 					]
 				}
-			}	
+			},
+
+			// collects all script tags and caches them for minification
+			dom_munger: {
+				client: {
+					options: {
+						read: {
+							selector: 'script',
+							attribute: 'src',
+							writeto: 'javascripts',
+							isPath: true
+						},
+						remove: ['script'],
+						append: {
+							selector: 'head',
+							html: '<script src="client.full.min.js"></script>'
+						} 
+					},
+					src: 'index.html',
+					dest: '<%= variables.dist %>/index.html'
+				}
+			},
+
+			//minifies scripts collected by the dom_munger
+			uglify: {
+				client: {
+					src: '<%= dom_munger.data.javascripts %>',		
+					dest: '<%= variables.dist %>/client.full.min.js'
+				}
+			},
+
+			//compresses and moves HTML to dist
+			htmlmin: {
+				client: {
+					options: {
+						removeComments: true,
+						collapseWhitespace: true
+					},
+					files: [{
+						expand: true,
+						src: [
+							'<%= variables.dist %>/index.html',
+							'app/layout/*.html'	
+						],
+						dest: '<%= variables.dist %>'
+					}]
+				}
+			}
+
 		});
 
 		grunt.registerTask ('build', [
-			'bowerInstall:client'	
+			'clean:client',
+			'bowerInstall:client',
+			'dom_munger:client',
+			'uglify:client',
+			'htmlmin:client'
 		]);
 	};
 
